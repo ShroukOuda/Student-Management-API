5691bed9e2413848bc0b8e64e718f2a029dd0e92
ShroukOuda <s36568961@gmail.com>
2025-09-06 20:35:47 +0300

    add RateLimitingMiddlewares

---
+++ Middlewares/RateLimitingMiddleware.cs
@@ -0,0 +1,38 @@
+namespace Student_Management_API.Middlewares;
+
+public class RateLimitingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    private static int _counter = 0;
+    private static DateTime _lastRequestDate = DateTime.Now;
+
+    public RateLimitingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        _counter++;
+        if (DateTime.Now.Subtract(_lastRequestDate).Seconds > 10)
+        {
+            _counter = 1;
+            _lastRequestDate = DateTime.Now;
+            await _next(context);
+        }
+        else
+        {
+            if (_counter > 5)
+            {
+                _lastRequestDate = DateTime.Now;
+                await context.Response.WriteAsync("Rate Limit Exceeded");
+            }
+            else
+            {
+                _lastRequestDate = DateTime.Now;
+                await _next(context);
+            }
+        }
+    }
+}
